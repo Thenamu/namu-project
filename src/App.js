058ccbda8346111260c4f9c5ea56e59a395b21b2
Mi1esian <wsh6922@naver.com>
2024-06-28 12:56:36 +0900

    주현이 바보?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import MainView from "./views/MainView";
 
 const App = () => {
-  return <div>App</div>
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainView/>}/>
+    </Routes>
+  </BrowserRouter>
+  );
 };
 
 export default App;
